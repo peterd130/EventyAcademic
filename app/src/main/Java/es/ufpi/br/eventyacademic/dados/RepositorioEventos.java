88f25a1f56d34ac93ffb75c309f3f9ceff260262
laftel <rafahmmds@gmail.com>
2017-11-10 15:35:32 -0300

    Criacao de pacotes

---
+++ app/src/main/Java/es/ufpi/br/eventyacademic/dados/RepositorioEventos.java
@@ -0,0 +1,57 @@
+package es.ufpi.br.eventyacademic.dados;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class RepositorioEventos {
+	
+List<Evento> listaEve = new LinkedList<Evento>();
+	
+	/**
+	 * Dado um evento este � inserido na lista de eventos
+	 */
+	public void inserir(Evento u) {
+		listaEve.add(u);
+	}
+
+	/**
+	 * Retorna a lista de eventos
+	 */
+	public List<Evento> listar() {
+		return listaEve;
+	}
+	
+	/**
+	 * busca um evento pelo nome
+	 * @param nome nome do evento desejado  para procura
+	 * @return retorna o evento se ele existir
+	 */
+
+	public Evento buscar(String nome) {		
+		Evento evento=null;
+		//percorre toda a lista e checa se o evento existe
+		for(Evento u:listaEve){
+			if (u.getNome().equals(nome) ){
+				evento = u;
+				return evento;
+			}
+		}
+		return evento;
+	}
+	
+	
+	
+	/**
+	 * Remove um evento da base de dados
+	 * @param u Evento a ser removido
+	 */
+
+	public void remover(Evento u) {
+		listaEve.remove(u);
+		
+	}
+
+
+
+}
---
+++ app/src/main/Java/es/ufpi/br/eventyacademic/dados/Usuario.java
@@ -0,0 +1,8 @@
+package es.ufpi.br.eventyacademic.dados;
+
+/**
+ * Created by Laftel on 10/11/2017.
+ */
+
+public class Usuario {
+}
